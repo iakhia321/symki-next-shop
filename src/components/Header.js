787154c60574f4b6e5fa21128f149f3c741afb1f
Navi321 <49982747+Navi321@users.noreply.github.com>
2023-11-10 20:13:57 +0200

    continued working with Next.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,7 @@ export default function Header() {
                 <Link href="/">Home</Link>
             </li>
             <li>
-                <Link href="/about">About Us</Link>
+                <Link href="/about/about">About Us</Link>
             </li>
             <li>
                 <Link href="/blog/hello-world">Blog Post</Link>
