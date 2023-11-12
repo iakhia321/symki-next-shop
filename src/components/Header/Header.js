4d133f6b7b06bfe087c05dbe0dc135a022cf3517
Yaroslavl1s <lisinskijaroslav@gmail.com>
2023-11-12 20:01:15 +0300

    delet test 1

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,10 @@
 import React from 'react'
-import { FB } from './Components/FB/FB'
+
 
 export default function Header() {
     return (
         <>
-        <FB />
+        
         </>
     )
 }
