41c4efa4f3ae575458d8d08b99545ffaaa4304e5
Yaroslavl1s <lisinskijaroslav@gmail.com>
2023-11-19 18:39:01 +0300

    start make header

--- src/components/Header/Components/FB/FB.js
+++ src/components/Header/Components/FB/FB.js
@@ -1,36 +1,39 @@
 import React from 'react'
 import Link from 'next/link'
-import styles from './FB.module.sass'
+import './FB.sass'
+import instagramblack from './../../../../image/instagramblack.png'
 
 export function FB() {
     return (
         <header>
-            <div className={styles.fb}>FRIENDS BOX</div>
+        <div>
+            <div className="headerFB">FRIENDS BOX</div>
             <div>
                 <Link href="https://instagram.com/_friends_box?igshid=YTQwZjQ0NmI0OA==">
-                    <button></button>
+                    <button className="instagramblack" alt="Vector" src={instagramblack} ></button>
                 </Link>
                 <Link href="/LC/LC">
-                    <button>Log in</button>
+                    <button className="Login">Log in</button>
                 </Link>
                 <Link href="/LC/LC">
-                    <button>Cart</button>
+                    <button className="Cart">Cart</button>
                 </Link>
             </div>
-            <div>
+            <div className="headerFB__navbar">
                 <Link href="/">
-                    <button>Home</button>
+                    <button className="headerFB__navbar__Btn" >Home</button>
                 </Link>
                 <Link href="/shop/shop">
-                    <button>Shop All</button>
+                    <button className="headerFB__navbar__Btn">Shop All</button>
                 </Link>
                 <Link href="/about/about">
-                    <button>About Us</button>
+                    <button className="headerFB__navbar__Btn">About Us</button>
                 </Link>
                 <Link href="/contact/contact">
-                    <button>Contact</button>
+                    <button className="headerFB__navbar__Btn">Contact</button>
                 </Link>
             </div>
+        </div>
         </header>
     )
 }
