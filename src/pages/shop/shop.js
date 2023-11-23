88daf40eae8934cff31946c952380f4c9b7a08f4
Yaroslavl1s <lisinskijaroslav@gmail.com>
2023-11-23 22:00:47 +0300

    line price

--- src/pages/shop/shop.js
+++ src/pages/shop/shop.js
@@ -9,17 +9,17 @@ export default function shopall() {
                 <p>Use slider or enter min and max price</p>
             </header>
             <div className="price-input">
-                <div className="field">
+                <div className="price-input__field">
                     <span>Min</span>
                     <input type="number" class="input-min" value="100"></input>
                 </div>
-                <div className="separator">-</div>
-                <div className="field">
+                <div className="price-input__separator">-</div>
+                <div className="price-input__field">
                     <span>Max</span>
                     <input type="number" class="input-min" value="2000"></input>
                 </div>
-                <div class="slider">
-                    <div className="progress"></div>
+                <div class="price-input__slider">
+                    <div className="slider__progress"></div>
                 </div>
             </div>
         </div>
