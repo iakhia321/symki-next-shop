4a78285a78214178e4f4bcb51fd1d0cf7afcff4a
Max <fedukhinm@mail.ru>
2023-11-23 21:28:24 +0300

    jopa kakaya to

--- src/pages/about/about.js
+++ src/pages/about/about.js
@@ -1,33 +1,17 @@
 import styles from './about.sass'
 
+
+
 export default function About() {
-    return ( <div className={styles.about}>About</div>,
-    <div>
-    <div className="TextS">
-            <h4>OUR STORY</h4>
-        </div>
-
-        <div className="TheB">
-            <h4>THE BRAND</h4>
-        </div>
-            <p className="TheB__BRAND">I'm a paragraph. Click here to add your own text and edit me. It’s easy. click “Edit Text” or double click me to
-                add your own content and make changes to the font.
-                Feel free to drag and drop me anywhere you like on your page. I’m a great place for you to tell a story and let your users know a little more about you.
-            </p>
-        
-        
+    return ( <div class="card">
+    <div class="img"></div>
+    <div class="content">
+        <h3>Science</h3>
+        <p>The science of today is the technology of tomorrow – Edward Teller.The science of today is the technology of tomorrow – Edward Teller.The science of today is the technology of tomorrow – Edward Teller.The science of today is the technology of tomorrow – Edward Teller</p>
+        <a href="#">
+            <button>Explore</button>
+        </a>
     </div>
-
-    
-    
-    
-    
-    
-    
-    
-    
+</div>
     )
-    
-    
-    
 }
