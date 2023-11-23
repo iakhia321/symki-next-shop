584efb1c82f1beb16740060f9b5794fd1547aaaf
Navi321 <49982747+Navi321@users.noreply.github.com>
2023-11-23 19:20:41 +0200

    attempt fix errors x2

--- src/pages/strpol/strpol.js
+++ src/pages/strpol/strpol.js
@@ -1,11 +1,7 @@
 import './strpol.sass'
 
 export default function StrPol() {
-    return(
-        <div className='storeP'>
-            <div className='storeP__slogan'>
-                <h1>Store Policy</h1>
-            </div>
-        </div>
+    return (
+        <div></div>
     )
 }
