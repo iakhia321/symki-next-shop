cb0edc733ea02e9a01d88aebbe88355e21f9bc4d
Yaroslavl1s <lisinskijaroslav@gmail.com>
2023-11-26 19:50:00 +0300

    Price Range Slider

--- src/pages/shop/shop.js
+++ src/pages/shop/shop.js
@@ -2,27 +2,10 @@ import './shop.sass';
 
 export default function shopall() {
     return(
-    <div className="dom">
-        <div className="wrapper">
-            <header>
-                <h2>Цена</h2>
-                <p>Use slider or enter min and max price</p>
-            </header>
-            <div className="price-input">
-                <div className="price-input__field">
-                    <span>Min</span>
-                    <input type="number" class="input-min" value="100"></input>
-                </div>
-                <div className="price-input__separator">-</div>
-                <div className="price-input__field">
-                    <span>Max</span>
-                    <input type="number" class="input-min" value="2000"></input>
-                </div>
-                <div class="price-input__slider">
-                    <div className="slider__progress"></div>
-                </div>
-            </div>
-        </div>
+    <div class="price-range-container">
+        <label for="priceRange">Price Range:</label>
+        <input type="range" id="priceRange" min="0" max="100" step="1" value="50" />
+        <p id="priceValue">50</p>
     </div>
     )
 }
