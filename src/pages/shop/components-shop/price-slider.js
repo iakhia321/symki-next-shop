9e4049904f3caf829ee8226795b7385b9d124b43
Yaroslavl1s <lisinskijaroslav@gmail.com>
2023-11-30 20:26:28 +0300

    3 btn

--- src/pages/shop/components-shop/price-slider.js
+++ src/pages/shop/components-shop/price-slider.js
@@ -24,31 +24,40 @@ const PriceSlider = () => {
         <h3>Shop-all</h3>
 
     
-    <div className="Price">
-      <p>Price Range: ${priceRange[0]} - ${priceRange[1]}</p>
-      <Slider
-        className="Slider-price"
-        range
-        min={0}
-        max={100}
-        value={priceRange}
-        onChange={handlePriceChange}
-      />
-      <InputNumber
-        className="InputNumber"
-        style={{ margin: '0 16px' }}
-        value={priceRange[0]}
-        onChange={(value) => handleInputChange(value, 0)}
-      />
-      -
-      <InputNumber
-        className="InputNumber2"
-        style={{ margin: '0 16px' }}
-        value={priceRange[1]}
-        onChange={(value) => handleInputChange(value, 1)}
-      />
+      <div className="Price">
+        <p>Price Range: ${priceRange[0]} - ${priceRange[1]}</p>
+        <Slider
+         className="Slider-price"
+         range
+         min={0}
+         max={100}
+         value={priceRange}
+         onChange={handlePriceChange}
+        />
+        <InputNumber
+         className="InputNumber"
+         style={{ margin: '0 16px' }}
+         value={priceRange[0]}
+         onChange={(value) => handleInputChange(value, 0)}
+        />
+        -
+        <InputNumber
+         className="InputNumber2"
+         style={{ margin: '0 16px' }}
+         value={priceRange[1]}
+         onChange={(value) => handleInputChange(value, 1)}
+        />
+      </div>
+      <div className="leftbar">
+        <div className="leftbar__symki">
+        <button className="symki">Symki</button>
+        <button className="symki">Korzina</button>
+        <button className="symki">Paket</button>
+        </div>
+      </div>
     </div>
-    </div>
+      
+      
   );
 };
 
