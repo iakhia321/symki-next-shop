7d7d0364367afc9d41d006d0380884e8bc3d608b
Navi321 <49982747+Navi321@users.noreply.github.com>
2023-12-01 13:33:04 +0200

    fix error in styles(shop.sass, slider.sass) maybe fix deploy in vercel

--- src/pages/shop/shop.js
+++ src/pages/shop/shop.js
@@ -1,27 +1,11 @@
 import './shop.sass';
 import PriceSlider from './components-shop/price-slider'
-
-
 import React, { useState } from 'react';
 
 export default function shopall () {
     return(
     <div>
         <PriceSlider></PriceSlider>
-import React, { useState } from 'react';
-
-const PriceRangeSlider = () => {
-    const [price, setPrice] = useState(50);
-
-    const handlePriceChange = (e) => 
-        setPrice(e.target.value);
-    };
-export default function shopall () {
-    return(
-    <div class="price-range-container">
-        <label for="priceRange">Price Range:</label>
-        <input type="range" id="priceRange" min="0" max="100" step="1" value="50" />
-        <p id="priceValue">50</p>
     </div>
     )
 }
