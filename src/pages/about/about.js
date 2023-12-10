5ab4093b4441f8d26b370258024e795e973a51d9
iakhia321 <hakievaha@gmail.com>
2023-12-10 20:57:48 +0300

    3в

--- src/pages/about/about.js
+++ src/pages/about/about.js
@@ -1,17 +1,22 @@
-import styles from './about.sass'
+import { Suspense } from 'react';
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
+import { MeshBasicMaterial } from 'three';
 
+import Model from "../../model/basket";
 
+const About = () => (
+  <Canvas camera={{ position: [0, 2, 5], zoom: 1 }} style={{ height: '500px', width: '100%', outline: '1px solid black' }}>
+    <OrbitControls 
+    minDistance={3}  // Минимальное расстояние для зума
+    maxDistance={10} // Максимальное расстояние для зума
+    />
+    <hemisphereLight intensity={1.2} />
 
-export default function About() {
-    return ( <div class="card">
-    <div class="img"></div>
-    <div class="content">
-        <h3>Science</h3>
-        <p>The science of today is the technology of tomorrow – Edward Teller.The science of today is the technology of tomorrow – Edward Teller.The science of today is the technology of tomorrow – Edward Teller.The science of today is the technology of tomorrow – Edward Teller</p>
-        <a href="#">
-            <button>Explore</button>
-        </a>
-    </div>
-</div>
-    )
-}
+    <Suspense fallback={null}>
+      <Model material={new MeshBasicMaterial({ color: 'red' })} />
+    </Suspense>
+  </Canvas>
+);
+
+export default About;
