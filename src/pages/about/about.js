a0608f35c81e4b637dba9464d04573512766244f
Yaroslavl1s <lisinskijaroslav@gmail.com>
2023-12-10 22:19:34 +0300

    fix

--- src/pages/about/about.js
+++ src/pages/about/about.js
@@ -1,10 +1,3 @@
-import { Suspense } from 'react';
-import { Canvas } from "@react-three/fiber";
-import { OrbitControls } from "@react-three/drei";
-import { MeshBasicMaterial } from 'three';
-
-import Model from "../../model/basket";
-
 const About = () => (
   <div></div>
 );
