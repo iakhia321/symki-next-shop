0ecfc30129505b34e888eacc61664784bc42660c
Yaroslavl1s <lisinskijaroslav@gmail.com>
2023-12-10 21:57:48 +0300

    Revert "Merge pull request #106 from Yaroslavl1s/test-1"
    This reverts commit 6a7e3eb741ceda6772e4518a3edee1e332a5e282, reversing
    changes made to 8fd6bd0812bb1df7613965f16befc41d4a8aa0c9.

---
+++ src/components/Shopall/Shop.js
@@ -0,0 +1,42 @@
+import Link from 'next/link';
+import Image from 'next/image';
+import sumka5 from '../../image/symka5.jpg';
+import sumka1 from '../../image/symka1.jpg';
+
+const Shop = () => {
+  return (
+    <>
+      <a href="https://www.example.com">
+        <Image src={sumka5} alt="sumka" width={300} height={200} />
+      </a>
+
+      <a href="https://www.example.com">
+        <Image src={sumka1} alt="sumka" width={300} height={200} />
+      </a>
+
+      <div className='menu'>
+        <ul className='shop_menu'>
+          <li>
+            <Link href="/home">
+              <a>All Products</a>
+            </Link>
+          </li>
+
+          <li>
+            <Link href="/home">
+              <a>Bags</a>
+            </Link>
+          </li>
+
+          <li>
+            <Link href="/home">
+              <a>Basket</a>
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </>
+  );
+};
+
+export default Shop;
