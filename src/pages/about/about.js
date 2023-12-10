734cc0f2ef98f3aad01fd0adcfaba828abcc6408
Yaroslavl1s <lisinskijaroslav@gmail.com>
2023-12-10 21:58:14 +0300

    bigger problem

--- src/pages/about/about.js
+++ src/pages/about/about.js
@@ -6,17 +6,7 @@ import { MeshBasicMaterial } from 'three';
 import Model from "../../model/basket";
 
 const About = () => (
-  <Canvas camera={{ position: [0, 2, 5], zoom: 1 }} style={{ height: '500px', width: '100%', outline: '1px solid black' }}>
-    <OrbitControls 
-    minDistance={3}  // Минимальное расстояние для зума
-    maxDistance={10} // Максимальное расстояние для зума
-    />
-    <hemisphereLight intensity={1.2} />
-
-    <Suspense fallback={null}>
-      <Model material={new MeshBasicMaterial({ color: 'red' })} />
-    </Suspense>
-  </Canvas>
+  <div></div>
 );
 
 export default About;
